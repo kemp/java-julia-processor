ccfc166e218ae7cde8f5e292e60d53ecee43eee7
Steven Kemp <github2@steven.globaloffset.com>
2019-10-31 17:00:55 -0400

    Add course information to top of every class

--- src/main/java/me/javajuliaprocessor/Token.java
+++ src/main/java/me/javajuliaprocessor/Token.java
@@ -1,3 +1,10 @@
+/*
+ * Class:       CS 4308 Section 2
+ * Term:        Fall 2019
+ * Name:        Cody Ziegler, Steven Kemp
+ * Instructor:   Deepa Muralidhar
+ * Project:  Deliverable 2 Parser - Java
+ */
 package me.javajuliaprocessor;
 
 class Token {
