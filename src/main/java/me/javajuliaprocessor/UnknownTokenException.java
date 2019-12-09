74803169901c318f9bc9fb50894f5b8c425ddf40
Steven Kemp <github2@steven.globaloffset.com>
2019-12-08 19:51:31 -0500

    Add required comments to top of files

--- src/main/java/me/javajuliaprocessor/UnknownTokenException.java
+++ src/main/java/me/javajuliaprocessor/UnknownTokenException.java
@@ -1,3 +1,11 @@
+/*
+ * Class:       CS 4308 Section 2
+ * Term:        Fall 2019
+ * Name:        Steven Kemp, Cody Ziegler
+ * Instructor:   Deepa Muralidhar
+ * Project:  Deliverable 1 Scanner - Java
+ */
+
 package me.javajuliaprocessor;
 
 class UnknownTokenException extends Exception {
